eac95e2848114f4173e73add138d24e6259eec0b
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-23 14:23:09 -0300

    FindById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,15 +10,15 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000d, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
+
+
 
 
     }
