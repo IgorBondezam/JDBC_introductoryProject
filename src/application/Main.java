05934e61e0922402abe70618a4fe5b128ff4bc61
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-23 13:56:52 -0300

    add entity Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
 import Entities.Department;
+import Entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +11,10 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000d, obj);
+
+        System.out.println(seller);
 
 
     }
