da60db8baf1b68dea430c9397d14b2925515d012
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-23 14:00:50 -0300

    Create department and seller DAO (interfaces)

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import Entities.Department;
+import Entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
